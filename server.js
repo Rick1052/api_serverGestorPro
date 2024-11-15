0962887df7cfff3e4721f069a6ff09c1a89fd25d
Henrique Santos <141459753+Rick1052@users.noreply.github.com>
2024-11-15 19:32:46 -0300

    server.js

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const cors = require("cors");
+// server.js - Arquivo principal do servidor
+const express = require('express'); // Importação do framework Express
+const cors = require("cors"); // Middleware para habilitar CORS
 const customerRoutes = require('./routes/customersRoute'); // Importa as rotas de "customers"
 const productRoutes = require('./routes/productRoute'); // Importa as rotas de "product"
 const pdvRoutes = require('./routes/pdvRoutes'); // Importa as rotas de "pdv"
 
-const app = express();
-const PORT = 8000;
+const app = express(); // Instância do aplicativo Express
+const PORT = 8000; // Definição da porta do servidor
 
+// Middleware global para habilitar CORS e processamento de JSON
 app.use(cors());
 app.use(express.json());
 
+// Rota principal do servidor
 app.get('/', (req, res) => {
     res.send("Servidor rodando");
 });
@@ -23,6 +26,7 @@ app.use('/product', productRoutes);
 // Define as rotas de "pdv" usando o roteador
 app.use('/pdv', pdvRoutes);
 
+// Inicialização do servidor na porta definida
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
